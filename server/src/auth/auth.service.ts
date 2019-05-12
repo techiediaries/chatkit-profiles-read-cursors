81374485a26c62dec3a565a9222d0cf74fbb8726
Techiediaries <22133314+techiediaries@users.noreply.github.com>
2019-05-12 23:23:23 +0000

    Update auth.service.ts

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -13,15 +13,15 @@ export class AuthService {
       private readonly jwtService: JwtService
     ) {
       this.chatkit = new Chatkit({
-      	instanceLocator: "v1:us1:8974881e-3870-47b4-9053-14dad6c0e314",
-      	key: "b801e52a-971d-42b5-8c60-99636dd974b9:hIpHVBedpyNkmlGtH4w3xuh/9L3t5yKVh4BfUFQF/5c="
+      	instanceLocator: "YOUR_INSTANCE_LOCATOR",
+      	key: "YOUR_SECRET_KEY"
 
       })    
     }
     private async createUser(userData: User): Promise<User>{
         return this.userService.create(userData).then((user: User) =>{
           const userId = `${user.name}${user.id}`;
-          const roomId = "19376018";
+          const roomId = "YOUR_ROOM_ID";
           const avatarURL = "http://127.0.0.1:3000/uploads/avatar.png";
           
           return this.chatkit.createUser({id: userId, 
